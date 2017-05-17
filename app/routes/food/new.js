199808ffe51eae4ab82389979be3e537e767ca63
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2017-05-16 21:50:05 -0400

    added logic within routes to fetch remote data

--- app/routes/food/new.js
+++ app/routes/food/new.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model() {
-      return this.get('store').createRecord('food-item', {
-        name: 'What are you craving?',
-        location: 'Where did you wanna eat it?'
-      });
-    }
+  model() {
+    return this.get('store').createRecord('food-item');
+  },
+  deactivate() {
+    this.get('controller.model').rollbackAttributes();
+  }
 });
