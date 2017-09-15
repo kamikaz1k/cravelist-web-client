6255a7d5f552bd1e7d4fdde6a92eea9eb686ee7f
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2017-09-14 22:17:52 -0400

    removed unnecessary actions

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,18 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	session: Ember.inject.service('session'),
-
-	actions: {
-    login() {
-      this.get('session').authenticate(
-        'authenticator:oauth2',
-        'kaiser.dandangi@gmail.com',
-        '1234567890'
-      ).catch(e => this.set('errorMessage', e));
-    },
-		logout() {
-			this.get('session').invalidate();
-		}
-	}
 })
